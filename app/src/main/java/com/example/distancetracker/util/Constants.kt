8d9ba5e17c167e256390cb77d3d5a9e9b5dd6e12
Damian9696 <damian.ogorek.1996@gmail.com>
2021-05-10 22:44:55 +0200

    Created new constants;

--- app/src/main/java/com/example/distancetracker/util/Constants.kt
+++ app/src/main/java/com/example/distancetracker/util/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
 
     const val PERMISSION_LOCATION_REQUEST_CODE = 1
     const val PERMISSION_BACKGROUND_LOCATION_REQUEST_CODE = 2
+
+    const val SERVICE_START = "SERVICE_START"
+    const val SERVICE_STOP = "SERVICE_STOP"
 }
