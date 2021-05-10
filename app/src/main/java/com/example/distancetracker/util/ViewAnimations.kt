bc9ee22a230eec500c7f1efa2dacfbac0ca8445e
Damian9696 <damian.ogorek.1996@gmail.com>
2021-05-10 22:18:39 +0200

    Added animation, and updated previous;

--- app/src/main/java/com/example/distancetracker/util/ViewAnimations.kt
+++ app/src/main/java/com/example/distancetracker/util/ViewAnimations.kt
@@ -1,11 +1,54 @@
 package com.example.distancetracker.util
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.animation.ObjectAnimator.ofFloat
+import android.animation.PropertyValuesHolder
 import android.view.View
+import androidx.core.view.isVisible
 
 fun View.fadeAnimation(alpha: Float, durationMilli: Long) {
-    ofFloat(this, View.ALPHA, alpha).apply {
-        duration = durationMilli
-        start()
+
+    this.let { view ->
+        if (alpha > 0) {
+            view.alpha = 0f
+            view.isVisible = true
+        }
+
+        ofFloat(this, View.ALPHA, alpha).apply {
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    if (alpha < 1) {
+                        view.isVisible = false
+                        view.alpha = 0f
+                    }
+                }
+            })
+            duration = durationMilli
+            start()
+        }
+    }
+}
+
+fun View.scaleXYAnimation(startScale: Float, endScale: Float, durationMilli: Long) {
+
+    this.let { view ->
+        view.scaleX = startScale
+        view.scaleY = startScale
+
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, endScale)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, endScale)
+        ObjectAnimator.ofPropertyValuesHolder(this, scaleX, scaleY)
+            .apply {
+                duration = durationMilli
+                addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        view.scaleX = startScale
+                        view.scaleY = startScale
+                    }
+                })
+                start()
+            }
     }
 }
