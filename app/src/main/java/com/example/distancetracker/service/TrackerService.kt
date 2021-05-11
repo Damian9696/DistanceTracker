572b01ed0534b30483be42d017ac2c308d05583e
Damian9696 <damian.ogorek.1996@gmail.com>
2021-05-11 22:21:23 +0200

    Provide dependencies for Service;

--- app/src/main/java/com/example/distancetracker/service/TrackerService.kt
+++ app/src/main/java/com/example/distancetracker/service/TrackerService.kt
@@ -1,20 +1,36 @@
 package com.example.distancetracker.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
 import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
+import com.example.distancetracker.util.Constants.NOTIFICATION_CHANNEL_ID
+import com.example.distancetracker.util.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.distancetracker.util.Constants.SERVICE_START
 import com.example.distancetracker.util.Constants.SERVICE_STOP
+import dagger.hilt.android.AndroidEntryPoint
 import java.lang.IllegalStateException
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class TrackerService : LifecycleService() {
 
+    @Inject
+    lateinit var notification: NotificationCompat.Builder
+
+    @Inject
+    lateinit var notificationManager: NotificationManager
+
     companion object {
-        val _started = MutableLiveData<Boolean>()
+        val started = MutableLiveData<Boolean>()
     }
 
     private fun initValues() {
-        _started.value = false
+        started.value = false
     }
 
     override fun onCreate() {
@@ -26,12 +42,23 @@ class TrackerService : LifecycleService() {
 
         intent?.let {
             when (it.action) {
-                SERVICE_START -> _started.value = true
-                SERVICE_STOP -> _started.value = false
+                SERVICE_START -> started.value = true
+                SERVICE_STOP -> started.value = false
                 else -> throw IllegalStateException("Action is not available. Action must be SERVICE_START or SERVICE_STOP.")
             }
         }
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun buildNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                IMPORTANCE_LOW
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
