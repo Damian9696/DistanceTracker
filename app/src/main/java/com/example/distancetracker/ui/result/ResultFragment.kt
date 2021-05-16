c91f2bcc9d86f8b37b6a24b91d43f5661ea0324d
Damian9696 <damian.ogorek.1996@gmail.com>
2021-05-16 17:50:12 +0200

    Implemented logic for share result;

--- app/src/main/java/com/example/distancetracker/ui/result/ResultFragment.kt
+++ app/src/main/java/com/example/distancetracker/ui/result/ResultFragment.kt
@@ -1,11 +1,13 @@
 package com.example.distancetracker.ui.result
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.example.distancetracker.R
 import com.example.distancetracker.databinding.FragmentResultBinding
 import com.example.distancetracker.ext.fadeAnimationWithDelay
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -30,6 +32,32 @@ class ResultFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         createAnimations()
         createValues()
+        createShareButton()
+    }
+
+    private fun createShareButton() {
+        binding.shareButton.setOnClickListener {
+            shareResult()
+        }
+    }
+
+    private fun shareResult() {
+        context?.let { context ->
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    String.format(
+                        context.getString(R.string.result_share_text),
+                        args.result.distance,
+                        args.result.time
+                    )
+                )
+            }
+            startActivity(shareIntent)
+        }
+
     }
 
     private fun createValues() {
