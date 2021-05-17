8afd20627e48c5075141e352129501e9c3489002
Damian9696 <damian.ogorek.1996@gmail.com>
2021-05-17 20:07:14 +0200

    Updated shapes class;

--- app/src/main/java/com/example/distancetracker/map/Shapes.kt
+++ app/src/main/java/com/example/distancetracker/map/Shapes.kt
@@ -2,15 +2,14 @@ package com.example.distancetracker.map
 
 import android.graphics.Color
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.ButtCap
-import com.google.android.gms.maps.model.JointType
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 
 class Shapes(private val googleMap: GoogleMap) {
 
+    var polylineList = mutableListOf<Polyline>()
+
     fun drawPolyline(locationList: MutableList<LatLng>) {
-        googleMap.addPolyline(
+        val polyline = googleMap.addPolyline(
             PolylineOptions().apply {
                 width(10f)
                 color(Color.BLUE)
@@ -20,5 +19,13 @@ class Shapes(private val googleMap: GoogleMap) {
                 addAll(locationList)
             }
         )
+
+        polylineList.add(polyline)
+    }
+
+    fun removeAllPolylines() {
+        for (polyline in polylineList) {
+            polyline.remove()
+        }
     }
 }
